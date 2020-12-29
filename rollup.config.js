a3e096346208ec329bd05ca437a9a6f96481564f
dairan <dairan@winning.com.cn>
2020-12-29 10:03:44 +0800

    rollup 配置完成

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+// ./`rollup.config.js`
+export default {
+  input: "./src/index.js",
+  output: [
+    {
+      format: "cjs",
+      file: "lib/bundle.cjs.js"
+    },
+    {
+      format: "es",
+      file: "lib/bundle.esm.js"
+    }
+  ]
+};
