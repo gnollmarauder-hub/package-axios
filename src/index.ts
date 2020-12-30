f87747ac5a379a9ee3af98714f036cbb1b52f811
dairan <dairan@winning.com.cn>
2020-12-30 15:27:21 +0800

    手动删除实现

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { axiosConfig, options } from './types/index'
-import { setHeaders } from './plugins/axios/axios.handleRequest'
-import handleResponse from './plugins/axios/axios.handleResponse'
+import { setHeaders, handleResponse, cancelToken } from './plugins/axios/axios.handleRequest'
 export default class adAxios {
   instance: any
   pureInstance: any
+  options: any
   constructor (options: axiosConfig) {
+    this.options = options
     this.create(options)
   }
 
@@ -14,7 +15,10 @@ export default class adAxios {
       baseURL: options.baseUrl,
       timeout: options.timeout
     })
+    this.pureInstance = axios
+    setHeaders(this.pureInstance)
     setHeaders(this.instance)
+    cancelToken(this.instance)
     handleResponse(this.instance)
   }
   static getCookieData () {}
@@ -46,7 +50,31 @@ export default class adAxios {
       })
     }
   }
-  getPureInstance () {
-    
+  getPureInstance (url: string, options: options) {
+    return params => {
+      if (options) {
+        options.data = params
+        options.params = params
+      }
+      return  new Promise ((resolve, reject) => {
+        this.pureInstance({
+          baseURL: this.options.baseUrl,
+          timeout: this.options.timeout,
+          url,
+          ...options
+        }).then (res => {
+          if (res.success && options.successTxt) {
+            console.log(options.successTxt)
+          } else {
+            if(options.failTxt) console.error(options.failTxt)
+          }
+          resolve(res)
+        }).catch (err => {
+          if(options.failTxt) console.trace(options.failTxt)
+          reject(err)
+        })
+      })
+    }
   }
+  cancelFn () {}
 }
--- src/plugins/axios/axios.handleRequest.ts
+++ src/plugins/axios/axios.handleRequest.ts
@@ -1,5 +1,8 @@
+import axios from 'axios'
+import handleError from './axios.handleError'
 import { getCookieData } from '../../util/index'
 
+let pendingPool = new Map()
 export function mergeHeader (config, headerData = {}) {
   config.headers.common = Object.assign(config.headers.common, headerData)
   return config
@@ -9,5 +12,45 @@ export function setHeaders(instance: any) {
     let { header } = getCookieData()
     config = mergeHeader(config, header)
     return config
+  }, err => {
+    return Promise.reject(err)
   })
+}
+export function cancelToken(instance: any) {
+  instance.interceptors.request.use(config => {
+    config.cancelToken = new axios.CancelToken(cancelFn => {
+      pendingPool.has(config.url) ? cancelFn(`${config.url}请求重复`) : pendingPool.set(config.url, {
+        cancelFn,
+        global: config.global
+      })
+    })
+    return config
+  }, err => {
+    return Promise.reject(err)
+  })
+}
+
+export function handleResponse (instance) {
+    instance.interceptors.response.use(res => {
+      const { config } = res
+      pendingPool.delete(config.url)
+      return res
+    }, err => {
+        const { config } = err
+        if (!err) return Promise.reject(err)
+        if (err.response) {
+            err = handleError(err)
+        } else {
+            // 没有response(没有状态码)的情况
+            // eg: 超时；断网；请求重复被取消；主动取消请求；
+            // 错误信息err传入isCancel方法，可以判断请求是否被取消
+            if (axios.isCancel(err)) {
+                throw new axios.Cancel(err.message || `请求'${config.url}'被取消`)
+            } else if (err.stack && err.stack.includes('timeout')) {
+                err.message = '请求超时!'
+            } else {
+                err.message = '连接服务器失败!'
+            }
+        }
+    })
 }
